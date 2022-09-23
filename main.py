77da4863b80c40105914e8dcfb2156fa06ef317a
fernando <sande.fernando.32656@gmail.com>
2022-09-23 12:43:03 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import time
+import uvicorn
+# Routers
+from vibraproject.routers import searcher
+
+
+# app definition
+app = FastAPI()
+
+# app config
+app.include_router(searcher.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+if __name__=="__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8095)
